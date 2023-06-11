b7baa3725543c29859d040a1d6e7b1d1a24eb0db
Farlaf <themehills@gmail.com>
2023-06-11 13:56:08 +0400

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :email, format: { with: /\A(.+)@(.+)\z/, message: 'email must include @' }
+  validates :email, uniqueness: true
 end
