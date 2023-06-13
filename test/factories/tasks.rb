43b0222d1cb09e9e70a45fd14ada61872e6a45ed
Farlaf <themehills@gmail.com>
2023-06-13 11:31:04 +0400

    fix test and factory for task

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description
     author_id { 1 }
     assignee_id { 1 }
-    state
+    state { '' }
     expired_at
   end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test 'create' do
     task = create(:task)
     assert task.persisted?
