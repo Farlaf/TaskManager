bf4449763fb8096b52bc1a0068ed3e62c5e7d993
Farlaf <themehills@gmail.com>
2023-06-17 18:16:33 +0400

    fix helper by rubocop

--- app/controllers/concerns/auth_helper.rb
+++ app/controllers/concerns/auth_helper.rb
@@ -18,6 +18,6 @@ module AuthHelper
   def current_user
     return if session[:user_id].blank?
 
-    @_current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
