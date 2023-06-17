3e4915b6e4060a8cbf95d1aec9fdba7470dd479b
Farlaf <themehills@gmail.com>
2023-06-17 12:58:30 +0400

    add new methods for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   scope module: :web do
     resource :board, only: :show
-    resource :session, only: :new
+    resource :session, only: [:new, :create, :destroy]
   end
 end
