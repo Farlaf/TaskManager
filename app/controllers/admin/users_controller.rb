381bbd58aacc6f437cb101054984e1f3d5e01203
Farlaf <themehills@gmail.com>
2023-06-23 17:37:31 +0400

    add create and update controllers for user in admin area

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,4 +15,28 @@ class Admin::UsersController < Admin::ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(admin_users_url)
+    else
+      render(:edit)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to(admin_users_url)
+    else
+      render(:edit)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :type)
+  end
 end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -19,12 +19,25 @@ class Admin::UsersControllerTest < ActionController::TestCase
 
   test 'should get edit' do
     user = create(:user)
-    get :edit, params: {id: user.id}
+    get :edit, params: { id: user.id }
     assert_response :success
   end
 
   test 'should get new' do
     get :new
     assert_response :success
+  end
+
+  test 'shold post create' do
+    user = attributes_for(:user)
+    post :create, params: { user: user }
+    assert_response :redirect
+  end
+
+  test 'should patch update' do
+    user = create(:user)
+    user_attrs = attributes_for(:user)
+    patch :update, params: { id: user.id, user: user_attrs }
+    assert_response :redirect
   end
 end
