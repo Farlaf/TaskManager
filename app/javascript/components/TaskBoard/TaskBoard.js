e38220f0f3c43467443d9bfc7b73e35856bf0e1a
Farlaf <themehills@gmail.com>
2023-07-02 12:17:33 +0400

    add kanban board

---
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Board from '@asseinfo/react-kanban';
+
+const data = {
+  columns: [
+    {
+      id: 1,
+      title: 'Backlog',
+      cards: [
+        {
+          id: 1,
+          title: 'Add card',
+          description: 'Add capability to add a card in a column',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Doing',
+      cards: [
+        {
+          id: 2,
+          title: 'Drag-n-drop support',
+          description: 'Move a card between the columns',
+        },
+      ],
+    },
+  ],
+};
+
+function TaskBoard() {
+  return <Board initialBoard={data} />;
+}
+
+export default TaskBoard;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from 'components/TaskBoard';
 
 Rails.start();
 ActiveStorage.start();
+WebpackerReact.setup({ TaskBoard });
