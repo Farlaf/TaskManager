e2d2d11d54df49053d119de8af479fbbf1734594
Farlaf <themehills@gmail.com>
2023-07-05 23:02:37 +0400

    add task and board from API

---
+++ app/javascript/components/Task/Task.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import useStyles from './useStyles';
+
+function Task({ task }) {
+  const styles = useStyles();
+
+  console.log(styles);
+
+  return (
+    <Card className={styles.root}>
+      <CardHeader title={task.name} />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {task.description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+Task.propTypes = {
+  task: PropTypes.shape().isRequired,
+};
+
+export default Task;
--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -1,36 +1,75 @@
-import React from 'react';
-import Board from '@asseinfo/react-kanban';
+import React, { useEffect, useState } from 'react';
+import KanbanBoard from '@asseinfo/react-kanban';
 import '@asseinfo/react-kanban/dist/styles.css';
+import { propOr } from 'ramda';
 
-const data = {
-  columns: [
-    {
-      id: 1,
-      title: 'Backlog',
-      cards: [
-        {
-          id: 1,
-          title: 'Add card',
-          description: 'Add capability to add a card in a column',
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Doing',
-      cards: [
-        {
-          id: 2,
-          title: 'Drag-n-drop support',
-          description: 'Move a card between the columns',
-        },
-      ],
-    },
-  ],
+import TasksRepository from '../../repositories/TasksRepository';
+import Task from '../Task';
+
+const STATES = [
+  { key: 'new_task', value: 'New' },
+  { key: 'in_development', value: 'In Dev' },
+  { key: 'in_qa', value: 'In QA' },
+  { key: 'in_code_review', value: 'in CR' },
+  { key: 'ready_for_release', value: 'Ready for release' },
+  { key: 'released', value: 'Released' },
+  { key: 'archived', value: 'Archived' },
+];
+
+const initialBoard = {
+  columns: STATES.map((column) => ({
+    id: column.key,
+    title: column.value,
+    cards: [],
+    meta: {},
+  })),
 };
 
 function TaskBoard() {
-  return <Board initialBoard={data} disableColumnDrag />;
+  const [board, setBoard] = useState(initialBoard);
+  const [boardCards, setBoardCards] = useState([]);
+
+  const loadColumn = (state, page, perPage) =>
+    TasksRepository.index({
+      q: { stateEq: state },
+      page,
+      perPage,
+    });
+
+  const loadColumnInitial = (state, page = 1, perPage = 10) => {
+    loadColumn(state, page, perPage).then(({ data }) => {
+      setBoardCards((prevState) => ({
+        ...prevState,
+        [state]: { cards: data.items, meta: data.meta },
+      }));
+    });
+  };
+
+  const generateBoard = () => {
+    const boardForGeneration = {
+      columns: STATES.map(({ key, value }) => ({
+        id: key,
+        title: value,
+        cards: propOr({}, 'cards', boardCards[key]),
+        meta: propOr({}, 'meta', boardCards[key]),
+      })),
+    };
+
+    setBoard(boardForGeneration);
+  };
+
+  const loadBoard = () => {
+    STATES.map(({ key }) => loadColumnInitial(key));
+  };
+
+  useEffect(() => loadBoard(), []);
+  useEffect(() => generateBoard(), [boardCards]);
+
+  return (
+    <KanbanBoard disableColumnDrag renderCard={(card) => <Task task={card} />}>
+      {board}
+    </KanbanBoard>
+  );
 }
 
 export default TaskBoard;
