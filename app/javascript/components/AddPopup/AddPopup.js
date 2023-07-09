83d486cde7f6401c2647c3db5c9a1dff650d3fef
Farlaf <themehills@gmail.com>
2023-07-09 12:32:26 +0400

    add popup and form

---
+++ app/javascript/components/AddPopup/AddPopup.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { has } from 'ramda';
+
+import TaskForm from 'forms/TaskForm';
+import useStyles from './useStyles';
+
+import Modal from '@material-ui/core/Modal';
+import { Button, Card, CardActions, CardContent, IconButton, TextField } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
+import CloseIcon from '@material-ui/icons/Close';
+
+function AddPopup({ onClose, onCardCreate }) {
+  const [task, changeTask] = useState(TaskForm.defaultAttributes());
+  const [isSaving, setSaving] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const handleCreate = () => {
+    setSaving(true);
+
+    onCardCreate(task).catch((error) => {
+      setSaving(false);
+      setErrors(error || {});
+
+      if (error instanceof Error) {
+        alert(`Creation Failed! Error: ${error.messsage}`);
+      }
+    });
+  };
+
+  const handleChangeTextField = (fieldName) => (event) => changeTask({ ...task, [fieldName]: event.target.value });
+
+  const styles = useStyles();
+
+  return (
+    <Modal className={styles.modal} open onClose={onClose}>
+      <Card className={styles.root}>
+        <CardHeader
+          action={
+            <IconButton onClick={onClose}>
+              <CloseIcon />
+            </IconButton>
+          }
+          title="Add New Task"
+        />
+        <CardContent>
+          <div className={styles.form}>
+            <TextField
+              error={has('name', errors)}
+              helperText={errors.name}
+              onChange={handleChangeTextField('name')}
+              value={task.name}
+              label="Name"
+              required
+              margin="dense"
+            />
+            <TextField
+              error={has('description', errors)}
+              helperText={errors.description}
+              onChange={handleChangeTextField('description')}
+              value={task.description}
+              label="Description"
+              required
+              margin="dense"
+            />
+          </div>
+        </CardContent>
+        <CardActions className={styles.actions}>
+          <Button disabled={isSaving} onClick={handleCreate} variant="contained" size="smal" color="promary">
+            Add
+          </Button>
+        </CardActions>
+      </Card>
+    </Modal>
+  );
+}
+
+AddPopup.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onCardCreate: PropTypes.func.isRequired,
+};
+
+export default AddPopup;
