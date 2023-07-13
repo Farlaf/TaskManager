04a583d0d4c3c709bbdaffcddd899686eb0068f2
Farlaf <themehills@gmail.com>
2023-07-13 12:16:35 +0400

    fix after rebase

--- app/javascript/components/EditPopup/components/Form/Form.js
+++ app/javascript/components/EditPopup/components/Form/Form.js
@@ -5,7 +5,7 @@ import { has } from 'ramda';
 import TextField from '@material-ui/core/TextField';
 
 import useStyles from './useStyles';
-import UserSelect from '../../UserSelect';
+import UserSelect from '../../../UserSelect';
 
 function Form({ errors, onChange, task }) {
   const handleChangeTextField = (fieldName) => (event) => onChange({ ...task, [fieldName]: event.target.value });
