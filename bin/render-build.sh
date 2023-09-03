dbb8e1dc5aca46c6e79dcee0a7c2a495e396ede9
Farlaf <themehills@gmail.com>
2023-09-03 15:55:56 +0400

    add db:seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,4 @@ bundle config set force_ruby_platform true
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:create db:migrate
+bundle exec rake db:create db:migrate db:seed
