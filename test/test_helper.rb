37109b3970fa654f7ee27fa96b674e25ca4bc828
Farlaf <themehills@gmail.com>
2023-09-07 17:37:46 +0400

    try to fix coverals

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,22 @@
 require 'simplecov'
 
-SimpleCov.start('rails') do
-  require 'simplecov-lcov'
+if ENV['CI']
+  SimpleCov.start('rails') do
+    if ENV['COVERAGE']
+      require 'simplecov-lcov'
 
-  SimpleCov::Formatter::LcovFormatter.config do |c|
-    c.report_with_single_file = true
-    c.single_report_path = 'coverage/lcov.info'
+      SimpleCov::Formatter::LcovFormatter.config do |c|
+        c.report_with_single_file = true
+        c.single_report_path = 'coverage/lcov.info'
+      end
+
+      formatter SimpleCov::Formatter::LcovFormatter
+
+    end
+
+    add_filter ['version.rb', 'initializer.rb']
+
   end
-
-  formatter SimpleCov::Formatter::LcovFormatter
-
-  add_filter ['version.rb', 'initializer.rb']
 end
 
 ENV['RAILS_ENV'] ||= 'test'
