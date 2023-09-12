c385c4f36af323f9fcd22c9d31dde3e9cd98b622
Farlaf <themehills@gmail.com>
2023-09-12 12:13:42 +0400

    remove helper

--- app/controllers/web/recover_passwords_controller.rb
+++ app/controllers/web/recover_passwords_controller.rb
@@ -1,7 +1,4 @@
 class Web::RecoverPasswordsController < Web::ApplicationController
-  include RecoverPasswordsHelper
-  before_action :get_user, :token_correct?, only: [:edit, :update]
-
   def new
     @recover = RecoverPasswordFormNew.new
   end
@@ -9,34 +6,33 @@ class Web::RecoverPasswordsController < Web::ApplicationController
   def create
     @recover = RecoverPasswordFormNew.new(recover_params)
 
-    if @recover.valid?
-      user = @recover.user
+    return render(:new) if @recover.invalid?
 
-      token = SecureRandom.urlsafe_base64
-      expire_time = 24.hour.from_now
-      user.update!({ reset_token: token, reset_expire: expire_time })
+    user = @recover.user
 
-      UserMailer.with({ user: user }).recover_password.deliver_now
+    token = SecureRandom.urlsafe_base64
+    expire_time = 24.hour.from_now
+    user.update!({ reset_token: token, reset_expire: expire_time })
 
-      flash[:notice] = 'Link to generate new password send to your email'
-      redirect_to(root_url)
-    else
-      render(:new)
-    end
+    UserMailer.with({ user: user }).recover_password.deliver_now
+
+    flash[:notice] = 'Link to generate new password send to your email'
+    redirect_to(root_url)
   end
 
   def edit
     @new_password = RecoverPasswordFormEdit.new
+    get_user_by_token
   end
 
   def update
     @new_password = RecoverPasswordFormEdit.new(new_pass_params)
+    @user ||= get_user_by_token
 
-    render(:edit) if !@new_password.valid?
+    return render(:edit) if @new_password.invalid?
 
     @user.update({
                    password: @new_password.password,
-                   password_confirmation: @new_password.password_confirmation,
                    reset_token: nil,
                    reset_expire: nil,
                  })
@@ -53,5 +49,13 @@ class Web::RecoverPasswordsController < Web::ApplicationController
 
   def new_pass_params
     params.require(:recover_password_form_edit).permit(:password, :password_confirmation)
+  end
+
+  def get_user_by_token
+    user = User.find_by(reset_token: params[:token])
+    return redirect_to(root_url, notice: 'user not found') if user.blank?
+    return redirect_to(new_recover_password_path, notice: 'incorrect token') if user.reset_expire < Time.now
+
+    user
   end
 end
