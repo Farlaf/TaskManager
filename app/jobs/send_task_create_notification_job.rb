e41c085d03a8dcea7a8eddb3bbd3451c9d6cb689
Farlaf <themehills@gmail.com>
2023-09-15 17:22:34 +0400

    change find_by to find

--- app/jobs/send_task_create_notification_job.rb
+++ app/jobs/send_task_create_notification_job.rb
@@ -1,6 +1,6 @@
 class SendTaskCreateNotificationJob < MailerJob
   def perform(task_id)
-    task = Task.find_by_id(task_id)
+    task = Task.find(task_id)
     return if task.blank?
 
     UserMailer.with(user: task.author, task: task).task_created.deliver_now
--- app/jobs/send_task_update_notification_job.rb
+++ app/jobs/send_task_update_notification_job.rb
@@ -1,6 +1,6 @@
 class SendTaskUpdateNotificationJob < MailerJob
   def perform(task_id)
-    task = Task.find_by(id: task_id)
+    task = Task.find(task_id)
     return if task.blank?
 
     UserMailer.with(user: task.author, task: task).task_updated.deliver_now
